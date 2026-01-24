1906e5e2b188e3874307ee5bd2ff42027ddc4d6c
Binaya764 <bigyanlimbu179@gmail.com>
2026-01-24 22:56:13 +0545

    adding routes

--- BackEnd/Server.js
+++ BackEnd/Server.js
@@ -1,5 +1,17 @@
 import express from "express";
 const app = express();
+app.get("/api/notes", (req,res)=>{
+    res.status(200).send("you got 20 notes");
+});
+app.post("/api/notes", (req,res)=>{
+    res.status(201).json({message: "Note created successfully!"});
+});
+app.put("/api/notes/:id", (req,res)=>{
+    res.status(200).send({message: "Note updated successfully!"});
+});
+app.delete("/api/notes/:id", (req,res)=>{
+    res.status(200).send({message: "Note deleted successfully!"});
+});
 app.listen(5000,()=>{
     console.log("server:5000");
-})
+});
