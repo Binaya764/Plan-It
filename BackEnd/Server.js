b86db30896f04564123f1072f17cff5aa6e30728
Binaya764 <bigyanlimbu179@gmail.com>
2026-01-24 23:08:20 +0545

    routes organized

--- BackEnd/Server.js
+++ BackEnd/Server.js
@@ -1,17 +1,7 @@
 import express from "express";
+import notesRoutes from "./routes/notesRoutes.js";
 const app = express();
-app.get("/api/notes", (req,res)=>{
-    res.status(200).send("you got 20 notes");
-});
-app.post("/api/notes", (req,res)=>{
-    res.status(201).json({message: "Note created successfully!"});
-});
-app.put("/api/notes/:id", (req,res)=>{
-    res.status(200).send({message: "Note updated successfully!"});
-});
-app.delete("/api/notes/:id", (req,res)=>{
-    res.status(200).send({message: "Note deleted successfully!"});
-});
+app.use("/api/notes", notesRoutes)
 app.listen(5000,()=>{
     console.log("server:5000");
 });
---
+++ BackEnd/routes/notesRoutes.js
@@ -0,0 +1,31 @@
+import express from "express";
+const router = express.Router();
+
+router.get("/",(req,res) =>{
+    res.status(200).send("you just fetched the notes");
+});
+
+router.post("/", (req,res)=>{
+   res.status(201).json({message: "Note created successfully!"});
+ });
+ router.put("/:id", (req,res)=>{
+     res.status(200).send({message: "Note updated successfully!"});
+ });
+ router.delete("/:id", (req,res)=>{
+   res.status(200).send({message: "Note deleted successfully!"});
+ });
+
+export default router;
+
+// app.get("/api/notes", (req,res)=>{
+//     res.status(200).send("you got 20 notes");
+// });
+// app.post("/api/notes", (req,res)=>{
+//     res.status(201).json({message: "Note created successfully!"});
+// });
+// app.put("/api/notes/:id", (req,res)=>{
+//     res.status(200).send({message: "Note updated successfully!"});
+// });
+// app.delete("/api/notes/:id", (req,res)=>{
+//     res.status(200).send({message: "Note deleted successfully!"});
+// });
